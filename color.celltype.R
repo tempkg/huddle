56a2bea9ff81e246ea3c7cbe82935a3ef2d0414e
Hua Sun <33794673+hsun9@users.noreply.github.com>
2025-04-18 15:55:26 -0400

    Add files via upload

---
+++ color.celltype.R
@@ -0,0 +1,77 @@
+
+
+colorF1 <- c(
+        "RGC" = "#d32222",
+        "CycProg" = "#f7d031",
+        "Astrocyte" = "#EB5800",
+        "OPC" = "#F781BF",
+        "nIPC" = "#ac67e6",
+        "Inhibitory" = "#1897ae",
+        "Excitatory" = "#1076b2",
+        "Neuron" = "#3498db",
+        "Neuroblast" = "#3e58d9",
+        "Oligodendrocyte" = "#7F7FCC",
+        "Ependymal cells" = "#0b877b",
+        "Microglia" = "#C11F9B",
+        "Immune" = "#6aae75",
+        "T cells" = "#8da7b3",
+        "CD8+ T" = "#cde9d2",
+        "CD4+ T" = "#b4d4ba",
+        "NK cells" = "#81a488",
+        "B cells" = "#94c79d",
+        "Plasma cells" = "#9bbdb6",
+        "Granulocyte" = "#8aa9a3",
+        "pDC" = "#99b99f",
+        "cDC" = "#acc8b1",
+        "Macrophage" = "#79a99d",
+        "Monocyte" = "#97b4ab",
+        "Blood" = "#000000",
+        "Fibroblast" = "#9E8215",
+        "EC" = "#919057",
+        "Tumor" = "#570146",
+        "Unclassified" = "#D1D1D1"
+        )
+
+colorS1 <- c(
+        "RGC" = "#d32222",
+        "CycProg" = "#f7d031",
+        "Astrocyte" = "#EB5800",
+        "OPC" = "#F781BF",
+        "nIPC" = "#ac67e6",
+        "Inhibitory" = "#1897ae",
+        "Excitatory" = "#1076b2",
+        "Neuron" = "#3498db",
+        "Neuroblast" = "#3e58d9",
+        "Oligo" = "#a8d47a",
+        "Epend" = "#0b877b",
+        "Microglia" = "#C11F9B",
+        "Immune" = "#6aae75",
+        "Blood" = "#000000",
+        "Fibro" = "#9E8215",
+        "EC" = "#919057",
+        "Tumor" = "#570146",
+        "Unknown" = "#D1D1D1"
+        )
+
+colorS2 <- c(
+        "RGC" = "#BC243C",
+        "CycProg" = "#EFC050",
+        "Astrocyte" = "#EF563C",
+        "OPC" = "#ff84c4",
+        "nIPC" = "#ac67e6",
+        "Inhibitory" = "#1897ae",
+        "Excitatory" = "#1076b2",
+        "Neuron" = "#0F4C81",
+        "Neuroblast" = "#3e58d9",
+        "Oligo" = "#a8d47a",
+        "Epend" = "#0b877b",
+        "Microglia" = "#cf53b2",
+        "Immune" = "#6aae75",
+        "Blood" = "#000000",
+        "Fibro" = "#9E8215",
+        "EC" = "#bfc9d5",
+        "Tumor" = "#570146",
+        "Unknown" = "#D1D1D1"
+        )
+
+
