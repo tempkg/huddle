d88c2314890e5fd2f83561ea086f417c08f59c53
Hua Sun <33794673+hsun9@users.noreply.github.com>
2026-01-11 13:00:27 -0500

    Add files via upload

--- 4.run.callScore.R
+++ 4.run.callScore.R
@@ -11,28 +11,42 @@ rds <- 'multiome.rds'
 f <- 'hs.EPN-ZR.xlsx'
 form <- 'sctype'
 
-out <- 'out_expMarkers.confirm.pdf'
+# 3x3 w=10,h=8
+w <- 10
+h <- 8
+
+out <- 'confirm_markers.epn-zr.pdf'
 
 GetoptLong(
     "f=s",      "file",
     "rds=s",    "rds file",
     "form=s",   "form style",
-    "outdir=s", "outdir"
+    "w=i",      "width",
+    "h=i",      "height",
+    "out=s",    "out pdf"
 )
 
-
-dir.create(outdir)
 
 seu <- readRDS(rds)
 seu <- SeuratAddScorePlus(obj=seu, fdata=f, form=form)
 
 
-df <- readxl::read_excel(f)
-features <- paste0(unique(df$cellName), '1')
+df <- NULL
+features <- NULL
+
+if (form == 'sctype'){
+    df <- readxl::read_excel(f)
+    features <- paste0(unique(df$cellName), '1')
+}
+if (form == 'seurat'){
+    df <- read.table(f, sep='\t', header=T)
+    features <- paste0(unique(df$cluster), '1')
+}
 
 # show features in UMAP
 p <- FeaturePlot(seu, features=features, reduction='wnn.umap', order=T, min.cutoff='q1')
-ggsave(out, w=10, h=8)
+# 3x3 image
+ggsave(out, w=w, h=h)
 
 
 
