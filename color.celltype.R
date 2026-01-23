61b0eec731c581d6b0b4626b3d9a832f16e1994c
Hua Sun <33794673+hsun9@users.noreply.github.com>
2026-01-22 21:04:42 -0500

    Add files via upload

---
+++ color.celltype.R
@@ -0,0 +1,108 @@
+
+
+colorF1 <- c(
+        "RGC" = "#d32222",
+        "CycProg" = "#f7d031",
+        "Astrocyte" = "#EB5800",
+        "OPC" = "#F781BF",
+        "nIPC" = "#ac67e6",
+        "Inhibitory" = "#1897ae",
+        "Excitatory" = "#1076b2",
+        "Neuron" = "#3498db",
+        "Neuroblast" = "#3e58d9",
+        "Oligodendrocyte" = "#7F7FCC",
+        "Ependymal cells" = "#0b877b",
+        "Microglia" = "#C11F9B",
+        "Immune" = "#6aae75",
+        "T cells" = "#8da7b3",
+        "CD8+ T" = "#cde9d2",
+        "CD4+ T" = "#b4d4ba",
+        "NK cells" = "#81a488",
+        "B cells" = "#94c79d",
+        "Plasma cells" = "#9bbdb6",
+        "Granulocyte" = "#8aa9a3",
+        "pDC" = "#99b99f",
+        "cDC" = "#acc8b1",
+        "Macrophage" = "#79a99d",
+        "Monocyte" = "#97b4ab",
+        "Blood" = "#000000",
+        "Fibroblast" = "#9E8215",
+        "EC" = "#919057",
+        "Tumor" = "#570146",
+        "Unclassified" = "#D1D1D1"
+        )
+
+colorS1 <- c(
+        "RGC" = "#d32222",
+        "CycProg" = "#f7d031",
+        "Astrocyte" = "#EB5800",
+        "OPC" = "#F781BF",
+        "nIPC" = "#ac67e6",
+        "Inhibitory" = "#1897ae",
+        "Excitatory" = "#1076b2",
+        "Neuron" = "#3498db",
+        "Neuroblast" = "#3e58d9",
+        "Oligo" = "#a8d47a",
+        "Epend" = "#0b877b",
+        "Microglia" = "#C11F9B",
+        "Immune" = "#6aae75",
+        "Blood" = "#000000",
+        "Fibro" = "#9E8215",
+        "EC" = "#919057",
+        "Tumor" = "#570146",
+        "Unknown" = "#D1D1D1"
+        )
+
+colorS2 <- c(
+        "RGC" = "#BC243C",
+        "CycProg" = "#EFC050",
+        "Astrocyte" = "#EF563C",
+        "Glioblast" = "#d7642c",
+        "OPC" = "#ff84c4",
+        "nIPC" = "#ac67e6",
+        "Inhibitory" = "#1897ae",
+        "Excitatory" = "#1076b2",
+        "Neuron" = "#0F4C81",
+        "Neuroblast" = "#3e58d9",
+        "Oligo" = "#E5E395",
+        "Epend" = "#4DAF4A",
+        "Microglia" = "#AED89C",
+        "Immune" = "#45B500",
+        "T-Cell" = "#9C8DFF",
+        "T-cell" = "#9C8DFF",
+        "B-Cell" = "#00C0B2",
+        "Blood" = "#000000",
+        "Fibro" = "#C7B37B",
+        "Fibroblast" = "#C7B37B",
+        "EC" = "#bfc9d5",
+        "Pericytes" = "#c2d8f2",
+        "Tumor" = "#B32B8C",
+        "Unknown" = "#D1D1D1"
+        )
+
+
+colorS3 <- c(
+        "RGC"="#BC243C",
+        "CycProg"="#3C8F82",
+        "Astrocyte"="#EF563C",
+        "OPC"="#B05128",
+        "nIPC"="#D42B77",
+        "Inhibitory"="#DF7CB7",
+        "Excitatory"="#C45690",
+        "Neuron"="#D96AAC",
+        "Oligo"="#FFD400",
+        "Epend"="#26A5C9",
+        "Microglia"="#1F6DB5",
+        "Immune"="#1F5384",
+        "T-Cell"="#247DD4",
+        "B-Cell"="#9BC4EE",
+        "Blood"="#000000",
+        "Fibroblast"="#C7B37B",
+        "EC"="#732A9C",
+        "Pericytes"="#A75AD3",
+        "Tumor"="#852255",
+        "Unknown"="#D1D1D1"
+        )
+
+
+
