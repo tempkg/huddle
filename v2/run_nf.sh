cd1b5340f06d440617b09d4ee2f9a8d88eb09567
Hua Sun <33794673+hsun9@users.noreply.github.com>
2026-02-10 15:46:57 -0500

    Add files via upload

---
+++ v2/run_nf.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# getOptions
+while getopts "s:" opt; do
+  case $opt in
+    s)
+      SEQ=$OPTARG ;;
+    ?) exit 1
+  esac
+done
+
+
+TIMESTAMP=$(date +%Y%m%d_%H%M%S)
+mkdir -p logs
+
+
+nohup conda run -n nf-env nextflow run main.nf \
+  --seq ${SEQ} \
+  -name "run_${TIMESTAMP}" \
+  -with-report "logs/report_${TIMESTAMP}.html" \
+  -with-trace "logs/trace_${TIMESTAMP}.txt" \
+  > "logs/nextflow_${TIMESTAMP}.log" 2>&1 &
+
+
+# Save PID
+echo $! > nextflow.pid
+echo "Nextflow running with PID: $!"
+echo "Check logs in: logs/"
+
