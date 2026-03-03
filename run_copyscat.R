0f85d4955b68b6ab38dc8e260900249b0835c636
Hua Sun <33794673+hsun9@users.noreply.github.com>
2026-03-03 13:10:03 -0500

    Add files via upload

---
+++ run_copyscat.R
@@ -0,0 +1,17 @@
+
+library(scvar)
+
+
+Run_CopyscAT(
+    ref_dir = 'hg38_references',
+    py_pfrag = 'src/process_fragment_file.py',
+    fragment = 'atac_fragments.tsv.gz',
+    fctrl = '',
+    outdir = 'out_copyscat'
+)
+
+
+
+
+
+
